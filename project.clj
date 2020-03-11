801425584580cb623884aaa69b70f7747f7f7252
Laszlo Domoszlai <dlacko@Laszlos-MBP.fritz.box>
2020-03-11 10:52:09 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject org.dlacko/async-unix-sockets "0.1.0"
-  :description "A node-ipc compaitble Clojure library for working with unix domain sockets using core.async channels."
-  :url "https://github.com/domoszlai/async-unix-sockets"
+  :description "A node-ipc compatible Clojure library for working with Unix Domain Sockets using core.async channels."
+  :url "https://github.com/domoszlai/clj-ipc"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[org.clojure/core.async "1.0.567"]
+                 [org.clojure/tools.logging "1.0.0"]
                  [com.kohlschutter.junixsocket/junixsocket-core "2.3.2"]
                  [cheshire "5.10.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [clj-time "0.8.0"]]
                    :global-vars {*warn-on-reflection* true}}})
