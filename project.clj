a69bedaf57e5d0915a49ad914d626641a21a4bb1
Laszlo Domoszlai <dlacko@Laszlos-MBP.fritz.box>
2020-03-27 16:22:52 +0100

    Don't sign with GPG

--- project.clj
+++ project.clj
@@ -3,8 +3,12 @@
   :url "https://github.com/domoszlai/clj-ipc"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.0.567"]
                  [org.clojure/tools.logging "1.0.0"]
                  [com.kohlschutter.junixsocket/junixsocket-core "2.3.2"]
-                 [cheshire "5.10.0"]])
+                 [cheshire "5.10.0"]]
+
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]])
